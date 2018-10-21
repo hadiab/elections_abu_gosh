849a9f8f5687517519128ca95d1f166ca995e3a4
Hadian <abdalhadi>
2018-10-21 11:23:02 +0000

    pull

--- config/app.php
+++ config/app.php
@@ -145,7 +145,6 @@ return [
         Illuminate\Session\SessionServiceProvider::class,
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
         /*
